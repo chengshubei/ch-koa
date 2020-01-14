4059b7c2908423d36ab3cfe7fbbefa4f46975895
chengshubei <csb@disanbo.com>
2020-01-14 19:48:38 +0800

    发布稳定版本1.2.0

--- format.js
+++ format.js
@@ -5,27 +5,16 @@ const log4js = require('log4js');
 const {BaseError, SystemError, NotFoundError} = require('ch-error');
 
 const Logger = log4js.getLogger('response');
-function create(statusCode = -1, message, error, data) {
-    let result = {};
-    result.code = statusCode;
-    result.data = data || null;
-    result.message = message ? String(message) : '';
-    result.error = error || '';
-    return result;
-}
-function success(data, message) {
-    let result = {};
-    result.code = 200;
-    result.data = data || null;
-    result.message = message || 'OK';
-    result.error = '';
-    return result;
-}
 
 module.exports = (app, config) => {
     app.use(async (ctx, next) => {
         try {
-            ctx.body = success();
+            ctx.body = {
+                code: 200,
+                data: null,
+                message: 'OK',
+                error: ''
+            };
             await next();
         } catch (e) {
             if (! (e instanceof BaseError)) {
@@ -43,7 +32,12 @@ module.exports = (app, config) => {
                 let messagePath = path.join(config.MESSAGE_PATH, ctx.get('lang'));
                 e.message = require(messagePath)[e.code] || e.message;
             }
-            let response = create(e.code, e.message, e.error, e.data);
+            let response = {
+                code: e.code || 500,
+                message: e.message || 'unknown reason',
+                data: e.data || null,
+                error: e.error || e.message || String(e)
+            };
             if (config.debug) {
                 Logger.warn(`*************DEBUG**************** \ntime: ${new Date().toLocaleString()}, ${ctx.url} operate ${ctx.method} request failed \nheader info: ${JSON.stringify(ctx.headers)} \nrequest body info: ${JSON.stringify(ctx.request.body)} \nresponse info: ${JSON.stringify(response)} \nerror name: ${e.name} \n`);
             }
